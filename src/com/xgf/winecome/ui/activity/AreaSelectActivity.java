c98f431101f1b6eb933404390bc9cae9d0992bd7
xgf <king.xgf@gmail.com>
2015-07-13 17:32:48 +0800

    person info

---
+++ src/com/xgf/winecome/ui/activity/AreaSelectActivity.java
@@ -0,0 +1,114 @@
+package com.xgf.winecome.ui.activity;
+
+import android.os.Bundle;
+import android.view.View;
+import android.view.View.OnClickListener;
+import android.widget.Button;
+
+import com.xgf.winecome.R;
+import com.xgf.winecome.ui.view.wheel.widget.OnWheelChangedListener;
+import com.xgf.winecome.ui.view.wheel.widget.WheelView;
+import com.xgf.winecome.ui.view.wheel.widget.adapters.ArrayWheelAdapter;
+
+public class AreaSelectActivity extends BaseWheelActivity implements
+		OnClickListener, OnWheelChangedListener {
+	private WheelView mViewProvince;
+	private WheelView mViewCity;
+	private WheelView mViewDistrict;
+	private Button mBtnConfirm;
+
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.person_info_form);
+		setUpViews();
+		setUpListener();
+		setUpData();
+	}
+
+	private void setUpViews() {
+		// mViewProvince = (WheelView) findViewById(R.id.id_province);
+		// mViewCity = (WheelView) findViewById(R.id.id_city);
+		// mViewDistrict = (WheelView) findViewById(R.id.id_district);
+		// mBtnConfirm = (Button) findViewById(R.id.btn_confirm);
+	}
+
+	private void setUpListener() {
+		// 添加change事件
+		mViewProvince.addChangingListener(this);
+		// 添加change事件
+		mViewCity.addChangingListener(this);
+		// 添加change事件
+		mViewDistrict.addChangingListener(this);
+		// 添加onclick事件
+		mBtnConfirm.setOnClickListener(this);
+	}
+
+	private void setUpData() {
+		initProvinceDatas();
+		mViewProvince.setViewAdapter(new ArrayWheelAdapter<String>(
+				AreaSelectActivity.this, mProvinceDatas));
+		// 设置可见条目数量
+		mViewProvince.setVisibleItems(7);
+		mViewCity.setVisibleItems(7);
+		mViewDistrict.setVisibleItems(7);
+		updateCities();
+		updateAreas();
+	}
+
+	@Override
+	public void onChanged(WheelView wheel, int oldValue, int newValue) {
+		// TODO Auto-generated method stub
+		if (wheel == mViewProvince) {
+			updateCities();
+		} else if (wheel == mViewCity) {
+			updateAreas();
+		} else if (wheel == mViewDistrict) {
+			mCurrentDistrictName = mDistrictDatasMap.get(mCurrentCityName)[newValue];
+			mCurrentZipCode = mZipcodeDatasMap.get(mCurrentDistrictName);
+		}
+	}
+
+	/**
+	 * 根据当前的市，更新区WheelView的信息
+	 */
+	private void updateAreas() {
+		int pCurrent = mViewCity.getCurrentItem();
+		mCurrentCityName = mCitisDatasMap.get(mCurrentProviceName)[pCurrent];
+		String[] areas = mDistrictDatasMap.get(mCurrentCityName);
+
+		if (areas == null) {
+			areas = new String[] { "" };
+		}
+		mViewDistrict
+				.setViewAdapter(new ArrayWheelAdapter<String>(this, areas));
+		mViewDistrict.setCurrentItem(0);
+	}
+
+	/**
+	 * 根据当前的省，更新市WheelView的信息
+	 */
+	private void updateCities() {
+		int pCurrent = mViewProvince.getCurrentItem();
+		mCurrentProviceName = mProvinceDatas[pCurrent];
+		String[] cities = mCitisDatasMap.get(mCurrentProviceName);
+		if (cities == null) {
+			cities = new String[] { "" };
+		}
+		mViewCity.setViewAdapter(new ArrayWheelAdapter<String>(this, cities));
+		mViewCity.setCurrentItem(0);
+		updateAreas();
+	}
+
+	@Override
+	public void onClick(View v) {
+		switch (v.getId()) {
+		// case R.id.btn_confirm:
+		// //showSelectedResult();
+		// break;
+		default:
+			break;
+		}
+	}
+
+}
