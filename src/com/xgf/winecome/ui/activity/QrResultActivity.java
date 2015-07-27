c1bad630e7151e7ff207875d93a3839a42850005
xgf <king.xgf@gmail.com>
2015-07-27 16:40:33 +0800

    QrResult

---
+++ src/com/xgf/winecome/ui/activity/QrResultActivity.java
@@ -0,0 +1,100 @@
+package com.xgf.winecome.ui.activity;
+
+import android.app.Activity;
+import android.content.Context;
+import android.content.Intent;
+import android.os.Bundle;
+import android.os.Handler;
+import android.os.Message;
+import android.view.View;
+import android.view.View.OnClickListener;
+import android.widget.EditText;
+import android.widget.LinearLayout;
+import android.widget.Toast;
+
+import com.xgf.winecome.R;
+import com.xgf.winecome.network.logic.UserLogic;
+
+public class QrResultActivity extends Activity implements OnClickListener {
+	private LinearLayout mQueryLl;
+
+	private EditText mPhoneEt;
+	private EditText mVerCodeEt;
+	private Context mContext;
+	// 登陆装填提示handler更新主线程，提示登陆状态情况
+	Handler mLoginHandler = new Handler() {
+
+		@Override
+		public void handleMessage(Message msg) {
+			int what = msg.what;
+			switch (what) {
+			case UserLogic.LOGIN_SUC: {
+				Intent intent = new Intent(QrResultActivity.this,
+						OrderListActivity.class);
+				startActivity(intent);
+				QrResultActivity.this.finish();
+				overridePendingTransition(R.anim.push_left_in,
+						R.anim.push_left_out);
+
+				break;
+			}
+			case UserLogic.LOGIN_FAIL: {
+				Toast.makeText(mContext, R.string.login_fail,
+						Toast.LENGTH_SHORT).show();
+				break;
+			}
+			case UserLogic.LOGIN_EXCEPTION: {
+				break;
+			}
+			case UserLogic.NET_ERROR: {
+				break;
+			}
+
+			default:
+				break;
+			}
+
+		}
+
+	};
+
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.order_query);
+		mContext = QrResultActivity.this;
+		setUpViews();
+		setUpListener();
+		setUpData();
+	}
+
+	private void setUpViews() {
+		mQueryLl = (LinearLayout) findViewById(R.id.order_query_submit_ll);
+
+		mPhoneEt = (EditText) findViewById(R.id.order_query_phone_et);
+		mVerCodeEt = (EditText) findViewById(R.id.order_query_ver_code_et);
+	}
+
+	private void setUpListener() {
+		mQueryLl.setOnClickListener(this);
+
+	}
+
+	private void setUpData() {
+	}
+
+	@Override
+	public void onClick(View v) {
+		switch (v.getId()) {
+
+		case R.id.order_query_submit_ll: {
+
+			break;
+		}
+
+		default:
+			break;
+		}
+	}
+
+}
