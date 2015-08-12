6c91ef5d04637c4d6d6b78b2164b8ff1878b9654
xgf <king.xgf@gmail.com>
2015-08-12 14:43:44 +0800

    new logo

--- src/com/xgf/winecome/ui/activity/HomeActivity.java
+++ src/com/xgf/winecome/ui/activity/HomeActivity.java
@@ -6,7 +6,6 @@ import android.content.DialogInterface;
 import android.content.DialogInterface.OnClickListener;
 import android.content.Intent;
 import android.os.Bundle;
-import android.util.Log;
 import android.view.View;
 import android.widget.CheckBox;
 import android.widget.CompoundButton;
--- src/com/xgf/winecome/ui/activity/ShopCartActivity.java
+++ src/com/xgf/winecome/ui/activity/ShopCartActivity.java
@@ -8,7 +8,6 @@ import android.content.Intent;
 import android.graphics.Color;
 import android.graphics.drawable.ColorDrawable;
 import android.os.Bundle;
-import android.util.Log;
 import android.util.TypedValue;
 import android.view.View;
 import android.view.View.OnClickListener;
