d3163a5d3a72a6640a5b9fc63b1dfa6028d19f9d
xgf <king.xgf@gmail.com>
2015-08-24 17:12:50 +0800

    wechat pay constants

--- src/com/xgf/winecome/pay/wxpay/simcpux/Constants.java
+++ src/com/xgf/winecome/pay/wxpay/simcpux/Constants.java
@@ -5,12 +5,13 @@ public class Constants {
 	// appid
 	// 请同时修改 androidmanifest.xml里面，.PayActivityd里的属性<data
 	// android:scheme="wxb4ba3c02aa476ea1"/>为新设置的appid
-	public static final String APP_ID = "wxf2f565574a968187";
+	public static final String APP_ID = "wx0a1bb37673d832fe";
 
 	// 商户号
-	public static final String MCH_ID = "1233848001";
+	public static final String MCH_ID = "1260897501";
 
+	//TODO
 	// API密钥，在商户平台设置
-	public static final String API_KEY = "412fde4e9c2e2bb619514ecea142e449";
+	public static final String API_KEY = "a7520f1edb7979318a6983e15a8a2d2b";
 
 }
