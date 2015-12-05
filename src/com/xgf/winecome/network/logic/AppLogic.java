f25a1dde35d72e9ff98b73c02b52a4e8a82b0d07
xgf <king.xgf@gmail.com>
2015-12-05 15:58:44 +0800

    fix bug

--- src/com/xgf/winecome/network/logic/AppLogic.java
+++ src/com/xgf/winecome/network/logic/AppLogic.java
@@ -48,10 +48,10 @@ public class AppLogic {
 
 					Log.e("xxx_versionid", ":" + URLEncoder.encode(versionNo, "UTF-8"));
 					Log.e("xxx_type", ":" + URLEncoder.encode("android", "UTF-8"));
-					rpc.addProperty("versionid",
-							URLEncoder.encode(versionNo, "UTF-8"));
 					rpc.addProperty("type",
 							URLEncoder.encode("android", "UTF-8"));
+					rpc.addProperty("versionid",
+							URLEncoder.encode(versionNo, "UTF-8"));
 
 					AndroidHttpTransport ht = new AndroidHttpTransport(
 							RequestUrl.HOST_URL);
